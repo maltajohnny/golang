3ca5c89f1bce4d22107dd1aceab297e56d628be7
Johnny Kalfeltz - Ext <johnny.kalfeltz.ext@brf.com>
2025-09-21 22:42:12 -0300

    Atividades: Criando e entendendo algumas funções avançadas

---
+++ Funcoes-Avancadas/Funções-Recursivas/recursivas.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
+
+func main() {
+
+	fmt.Println("Funções Recursivas")
+	
+	posicao := uint(12)
+
+	for i := uint(0); i < posicao; i++ {
+		fmt.Println(fibonacci(i))
+	}
+
+	//Fibonacci 1 1 2 3 5 8 13
+}
